2cab8de7813ff1efe13b9a191d1dbf79ac649e63
Aleksandar Dinic <a.dinic@icloud.com>
2020-04-12 15:45:06 +0200

    Create new Xcode project

---
+++ ComicsInfo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ComicsInfo
+//
+//  Created by Aleksandar Dinic on 12/04/2020.
+//  Copyright © 2020 Aleksandar Dinic. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
