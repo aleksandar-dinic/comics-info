c77b49ad5e5107ffd37690f9efa15ab241ac140c
Aleksandar Dinic <a.dinic@icloud.com>
2020-04-24 21:00:19 +0200

    Domain Package

---
+++ Domain/Sources/Domain/Series/Series.swift
@@ -0,0 +1,40 @@
+//
+//  Series.swift
+//  Domain
+//
+//  Created by Aleksandar Dinic on 23/04/2020.
+//  Copyright © 2020 Aleksandar Dinic. All rights reserved.
+//
+
+import Foundation
+
+public struct Series: Codable {
+
+    /// The unique ID of the series resource.
+    public let identifier: String
+
+    /// The value of Series popularity
+    public let popularity: Int
+
+    /// The canonical title of the series.
+    public let title: String
+
+    /// A description of the series.
+    public let description: String?
+
+    /// The first year of publication for the series.
+    public let startYear: Int?
+
+    /// The last year of publication for the series (conventionally, nil for ongoing series) .
+    public let endYear: Int?
+
+    /// The representative image for this series.
+    public let thumbnail: String?
+
+    /// A resource list containing characters which appear in comics in this series.
+    public let characters: [Character]?
+
+    /// ID of the series which follows this series.
+    public let nextIdentifier: String?
+
+}
