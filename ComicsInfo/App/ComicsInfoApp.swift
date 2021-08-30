ee9a4ec32b27d38d6ccd7bffb1f092b04681dc77
Aleksandar Dinic <a.dinic@icloud.com>
2021-08-30 20:56:31 +0200

    Add Firebase

--- ComicsInfo/App/ComicsInfoApp.swift
+++ ComicsInfo/App/ComicsInfoApp.swift
@@ -6,12 +6,24 @@
 //
 
 import SwiftUI
+import Firebase
+import GoogleMobileAds
 
 @main
 struct ComicsInfoApp: App {
+    
+    init() {
+        // Use Firebase library to configure APIs.
+        FirebaseApp.configure()
+
+        // Initialize the Google Mobile Ads SDK.
+        GADMobileAds.sharedInstance().start(completionHandler: nil)
+    }
+    
     var body: some Scene {
         WindowGroup {
             DiscoverView()
         }
     }
+    
 }
