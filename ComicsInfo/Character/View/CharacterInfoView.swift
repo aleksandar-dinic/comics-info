6c326ac435d55606ccdf62f25c3292b559ba1468
Aleksandar Dinic <a.dinic@icloud.com>
2021-09-07 19:58:56 +0200

    Add Character Info View

---
+++ ComicsInfo/Character/View/CharacterInfoView.swift
@@ -0,0 +1,22 @@
+//
+//  CharacterInfoView.swift
+//  ComicsInfo
+//
+//  Created by Aleksandar Dinic on 07/09/2021.
+//
+
+import SwiftUI
+
+struct CharacterInfoView: View {
+    
+    var body: some View {
+        Text("Hello, World!")
+    }
+    
+}
+
+struct CharacterInfoView_Previews: PreviewProvider {
+    static var previews: some View {
+        CharacterInfoView()
+    }
+}
